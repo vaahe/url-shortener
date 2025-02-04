f8cc6e3001b6e2474c329f5be2d9149b25c6a094
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2025-02-05 00:06:31 +0400

    incapsulate firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAFJJTxmsiF9_a7MEVcCBiKltYHUuYIGLQ",
-    authDomain: "url-shortener-2a3c7.firebaseapp.com",
-    projectId: "url-shortener-2a3c7",
-    storageBucket: "url-shortener-2a3c7.firebasestorage.app",
-    messagingSenderId: "655902537793",
-    appId: "1:655902537793:web:183e0e1eb14b4aa5906ec0",
-    measurementId: "G-9LPM3JHFHP"
-};
+import { firebaseConfig } from "../config";
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
